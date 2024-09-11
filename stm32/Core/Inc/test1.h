9eec94387f611847dd324580ad461c3ae97cde7a
Kacper <kacperszcz159@gmail.com>
2024-09-11 22:55:13 +0200

    Project tree update

---
+++ stm32/Core/Inc/test1.h
@@ -0,0 +1,14 @@
+/*
+ * test1.h
+ *
+ *  Created on: Sep 11, 2024
+ *      Author: kacpe
+ */
+
+#ifndef INC_TEST1_H_
+#define INC_TEST1_H_
+
+#define DUPA 100
+
+
+#endif /* INC_TEST1_H_ */
--- stm32/README.md
+++
@@ -1,2 +0,0 @@
-# SCARE-Qs
-Project of Robot Scara cooperating with conveyor based on Developer board STM32 - XXX, with Matlab model, ROS communication and SCADA based on Qt.
